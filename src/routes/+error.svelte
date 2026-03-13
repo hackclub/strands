22da166e0f62cd326281f404dd59a9c74e6d7960
EDRipper <euan@hackclub.com>
2026-03-13 15:47:47 -0400

    big bang

---
+++ src/routes/+error.svelte
@@ -0,0 +1,9 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+
+<img
+	src="https://http.cat/{$page.status}"
+	alt="HTTP {$page.status}"
+	style="display:block; max-width:500px; width:90%; margin:4rem auto;"
+/>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,22 @@
+<style>
+	@font-face {
+		font-family: 'OnlyTrue';
+		src: url('/fonts/OnlyTrue-Neutral.ttf') format('truetype');
+		font-stretch: normal;
+	}
+
+	h1 {
+		font-family: 'OnlyTrue', sans-serif;
+        font-size: 5vw;
+        line-height: 1;
+        white-space: nowrap;
+        color: #000 ;
+        font-weight: 1;
+
+        -webkit-font-smoothing: antialiased;
+
+        -moz-osx-font-smoothing: grayscale;
+    }
+</style>
+
+<h1>BUILD A BEEST</h1>
